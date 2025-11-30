eb3a73b576f49b6c97274b54736be559ed188142
DonyoBen <saquinbenjie7@gmail.com>
2025-12-01 03:38:20 +0800

    refactor auth button to use render hook

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -1,33 +1,20 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
-import { createClient } from "@/utils/supabase/client";
-//import { signout } from "@/lib/auth-actions";
+import { useUser } from "@/hooks/use-user";
+
 
 const AuthButton = () => {
-  const [user, setUser] = useState<any>(null);
-  const router = useRouter();
-  const supabase = createClient();
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
-      setUser(user);
-    };
-    fetchUser();
-  }, []);
+    const { user, signOut } = useUser();
+    const router = useRouter();
 
   if (user) {
     return (
       <>
         <Button
           onClick={() => {
-            //signout();
-            setUser(null);
+            signOut();
           }}
         >
           Log out
