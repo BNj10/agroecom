0b0b7c2d5fa4e8525a5108cee814fc487481270f
DonyoBen <saquinbenjie7@gmail.com>
2025-12-10 19:52:54 +0800

    add site-config to store contact info

---
+++ lib/site-config.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+    name: "AgroEcom",
+    description: "Agricultural Ecommerce Platform",
+    contact: {
+        email: "agroecom@gmail.com",
+        phone: "+63 961 886 2772", 
+        address: "Ormoc City, Leyte",
+        mapUrl: "https://www.google.com/maps/search/?api=1&query=Ormoc+City"
+    }
+}
